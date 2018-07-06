cb31225e3907c894295e10778419b3a395d4bf76
daThug <40901028+daThug@users.noreply.github.com>
2018-07-06 23:35:44 +0200

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,15 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+    console.log('I am ready!');
+});
+
+client.on('message', message => {
+    if (message.content === 'hi') {
+    	message.reply('hi , you smell like a shoe');
+  	}
+});
+
+// THIS  MUST  BE  THIS  WAY
+client.login(process.env.BOT_TOKEN);
